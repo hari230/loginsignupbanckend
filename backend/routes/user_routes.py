2ccc31c0866dad63f05e90af5e8f8f83046177b5
harshitha <harshithasamudrala2000@gmail.com>
2025-03-28 12:32:53 +0530

    "jd"

--- backend/routes/user_routes.py
+++ backend/routes/user_routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, jsonify
-from models import User
+# from models import User
+from backend.models import User
+
 from database import users_collection
 from auth import hash_password, verify_password, create_jwt_token
 
