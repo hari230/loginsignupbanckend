196f56860eb123c6b6084e0e0c325899db58d75b
harshitha <harshithasamudrala2000@gmail.com>
2025-03-28 12:25:48 +0530

    "app.py"

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
-from routes.user_routes import user_routes
+# from routes.user_routes import user_routes
+
+from backend.routes.user_routes import user_routes
+
 
 app = Flask(__name__)
 app.register_blueprint(user_routes, url_prefix="/auth")
